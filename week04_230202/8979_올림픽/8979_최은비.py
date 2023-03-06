d4e60fa945160452356e1091071d67d033760bd6
rloldl <you@example.comii319@naver.comgit config --global user.email you@example.comii319@naver.com>
2023-03-06 23:10:36 +0900

    8979_최은비

--- week04_230202/8979_올림픽/8979_최은비.py
+++ week04_230202/8979_올림픽/8979_최은비.py
@@ -8,7 +8,7 @@ ranking = [list(map(int, input().split())) for _ in range(num)]
 ranking_dict = {}
 ans = 0
 
-# 0번째 수는 국가 이름(?)이니까 빼고 정렬
+# 0번째 수는 국가 이름이니까 빼고 정렬
 ranking = sorted(ranking, key=lambda x: (x[1], x[2], x[3]), reverse=True)
 
 # 정렬된 리스트를 순회하면서 국가 이름(0번째 요소)을 키로, 각 매달 수를 밸류로 딕셔너리에 저장
