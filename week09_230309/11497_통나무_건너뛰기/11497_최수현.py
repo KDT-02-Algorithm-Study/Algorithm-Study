b433a7b98a400a2b71bbafe8259f1d9d3509133a
최수현 <sw92522@naver.com>
2023-03-09 18:04:29 +0900

    Add explanation_최수현

--- week09_230309/11497_통나무_건너뛰기/11497_최수현.py
+++ week09_230309/11497_통나무_건너뛰기/11497_최수현.py
@@ -7,7 +7,10 @@ for _ in range(t):
     n = int(input())
     trees = sorted(list(map(int, input().split())))
     
+    # 작은 통나무 두개와 가장 큰 통나무 두개의 길이차이 중 큰것
     level = max(trees[1]-trees[0], trees[-1]-trees[-2])
+
+    # 양쪽 번갈아가며 배치(인덱스 2씩 차이남)
     for i in range(2, n):
         if trees[i] - trees[i-2] > level:
             level = trees[i] - trees[i-2]
