a98789af075828d007e2a396f3f6d0b75b616010
Ssooob <121845110+Ssooob@users.noreply.github.com>
2023-05-25 19:58:58 +0900

    week19_이수빈

---
+++ week19_230525/11501_주식/11501_이수빈.py
@@ -0,0 +1,22 @@
+# 11501 주식
+# KB / ms 
+# 기본 코드
+import sys
+sys.stdin = open("input.txt", "r")
+input = sys.stdin.readline
+
+T = int(input())
+for _ in range(T):
+    N = int(input())
+    stock = list(map(int, input().split()))
+    stock = stock[::-1]
+    max = stock[0]
+    sum = 0
+
+    for i in range(1, N):
+        if max < stock[i]:
+            max = stock[i]
+            continue
+        sum += max - stock[i]
+
+    print(sum)
