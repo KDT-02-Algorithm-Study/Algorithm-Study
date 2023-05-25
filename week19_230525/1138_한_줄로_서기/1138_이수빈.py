d0f2a89bec9e58cf3868d3086b07eb6f68e20bbc
Ssooob <121845110+Ssooob@users.noreply.github.com>
2023-05-25 20:14:57 +0900

    Update 1138_이수빈.py

--- week19_230525/1138_한_줄로_서기/1138_이수빈.py
+++ week19_230525/1138_한_줄로_서기/1138_이수빈.py
@@ -1,6 +1,24 @@
 # 1138 한 줄로 서기
-# KB / ms 
+# 31256 KB / 40 ms 
 # 기본 코드
+
+
 import sys
 sys.stdin = open("input.txt", "r")
 input = sys.stdin.readline
+
+n = int(input())
+h = list(map(int, input().split()))
+ans = [0] * n
+
+for p in range(1, n+1):
+    t = h[p-1]
+    cnt = 0
+    for i in range(n):
+        if cnt == t and ans[i] == 0:
+            ans[i] = p
+            break
+        elif ans[i] == 0:
+            cnt += 1
+print(*ans)
+
